d4e023979b65b2299d0884bedd1cbfc862fdf134
kisar18 <martinpur99@seznam.cz>
2020-07-16 16:39:42 +0200

    Changing color with the hex name of the color

---
+++ script.js
@@ -0,0 +1,63 @@
+var button = document.getElementById('btn')
+
+function createColorSegment(value) {
+    const Segment = {}
+    Segment.value = value
+    return Segment
+}
+
+button.onclick = function() {
+    const r1 = createColorSegment(undefined)
+    const r2 = createColorSegment(undefined)
+    const g1 = createColorSegment(undefined)
+    const g2 = createColorSegment(undefined)
+    const b1 = createColorSegment(undefined)
+    const b2 = createColorSegment(undefined)
+
+    function HexadecimalColor(colorSegment) {
+        var numberOfColorSegment = Math.floor(Math.random() * 16)
+
+        if (numberOfColorSegment == 10) {
+            colorSegment.value = 'a'
+        }
+        else if (numberOfColorSegment == 11) {
+            colorSegment.value = 'b'
+        }
+        else if (numberOfColorSegment == 12) {
+            colorSegment.value = 'c'
+        }
+        else if (numberOfColorSegment == 13) {
+            colorSegment.value = 'd'
+        }
+        else if (numberOfColorSegment == 14) {
+            colorSegment.value = 'e'
+        }
+        else if (numberOfColorSegment == 15) {
+            colorSegment.value = 'f'
+        }
+        else {
+            colorSegment.value = numberOfColorSegment
+        }
+    }
+
+    HexadecimalColor(r1)
+    HexadecimalColor(r2)
+    HexadecimalColor(g1)
+    HexadecimalColor(g2)
+    HexadecimalColor(b1)
+    HexadecimalColor(b2)
+    
+    console.log(r1.value)
+    console.log(r2.value)
+    console.log(g1.value)
+    console.log(g2.value)
+    console.log(b1.value)
+    console.log(b2.value)
+    
+    
+    var HexColor = "#" + r1.value + r2.value + g1.value + g2.value + b1.value + b2.value
+    console.log(HexColor) 
+    
+    document.getElementsByTagName('body')[0].style.backgroundColor = HexColor
+    document.getElementsByClassName('hex-color-name')[0].innerText = "HEX COLOR: " + HexColor
+}
